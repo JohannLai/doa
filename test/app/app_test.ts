59e614e7ba4947c6704522cbb7f76fc4aa162439
lizhihang <johannli@tencent.com>
2020-06-16 21:55:35 +0800

    test: add example test

--- test/app/app_test.ts
+++ test/app/app_test.ts
@@ -1,11 +1,16 @@
 import {
   App,
 } from "../../mod.ts";
+import {
+  assertEquals
+} from "../deps.ts";
 
-Deno.test("app works", async function () {
+Deno.test("app should works", async function () {
   const body = "Hello, world!";
   const app = new App();
   app.use((ctx) => {
     ctx.body = body;
   });
+
+  assertEquals(3, 3);
 });
