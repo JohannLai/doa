904045ffc4dee6b71e436888cee8e7c828efe4cd
lizhihang <johannli@tencent.com>
2020-06-16 21:38:46 +0800

    fix: fix test

--- test/app/app_test.ts
+++ test/app/app_test.ts
@@ -1,10 +1,9 @@
 import {
   App,
 } from "../../mod.ts";
-import { assertEquals, superdeno } from "./../deps.ts";
 
 Deno.test("app works", async function () {
-  const body = "Hello, I'm ako 🦕!";
+  const body = "Hello, world!";
   const app = new App();
   app.use((ctx) => {
     ctx.body = body;
--- test/deps.ts
+++ test/deps.ts
@@ -5,5 +5,4 @@ export {
   assertNotEquals,
   assertThrows,
 } from "https://deno.land/std/testing/asserts.ts";
-export { superdeno } from "https://deno.land/x/superdeno@1.1.2/mod.ts";
 export * as path from "https://deno.land/std/path/mod.ts";
