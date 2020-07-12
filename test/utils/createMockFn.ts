b4bf1465f840716a8f1f13277ce1988001300ea5
johannlai <johannli@tencent.com>
2020-07-12 16:30:09 +0800

    test: add test utils for mock

---
+++ test/utils/createMockFn.ts
@@ -0,0 +1,47 @@
+import {
+  ServerRequest,
+  BufReader,
+  BufWriter,
+} from "../test_deps.ts";
+import { App } from "../../mod.ts";
+import { Context } from "../../context.ts";
+import { Request } from "../../request.ts";
+import { Response } from "../../response.ts";
+interface MockServerOptions {
+  headers?: [string, string][];
+  proto?: string;
+  url?: string;
+}
+
+export function createMockApp(): App {
+  return {} as any;
+}
+
+export function createMockServerRequest({
+  url = "/",
+  proto = "HTTP/1.1",
+  headers: headersInit = [["cache-control", "max-age=0"]],
+}: MockServerOptions = {}): ServerRequest {
+  const headers = new Headers(headersInit);
+  return {
+    conn: {
+      close() {},
+    },
+    r: new BufReader(new Deno.Buffer(new Uint8Array())),
+    w: new BufWriter(new Deno.Buffer(new Uint8Array())),
+    headers,
+    method: "GET",
+    proto,
+    url,
+    async respond() {},
+  } as any;
+}
+
+export function createMockCtx() {
+  return new Context(
+    createMockApp(),
+    createMockServerRequest(),
+    new Request(createMockServerRequest()),
+    new Response({ headers: new Headers() }),
+  );
+}
