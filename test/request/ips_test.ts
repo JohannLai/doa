c7d2000ad0b9e505862c25dcbffaae25b76b3631
johannlai <johann_lai@outlook.com>
2020-07-19 20:18:33 +0800

    test: add ips test

--- test/request/ips_test.ts
+++ test/request/ips_test.ts
@@ -25,29 +25,19 @@ test({
 });
 
 test({
-  name:
-    "req.ips, when X-Forwarded-For is present, and proxy is not trusted, should be ignored",
+  name: "req.ip, with no req.ips present, should return req.conn.remoteAddr",
   async fn() {
-    const mockServerRequest = createMockServerRequest();
+    const mockServerRequest = createMockServerRequest({
+      conn: {
+        close: {},
+        remoteAddr: { hostname: "127.0.0.2", port: 64859, transport: "tcp" },
+      },
+    });
 
     const request = new Request(mockServerRequest);
 
-    request.headers.set("x-forwarded-for", "127.0.0.1,127.0.0.2");
+    request.headers.set("x-forwarded-for", "127.0.0.1");
 
-    assertEquals(request.ips, []);
-  },
-});
-
-test({
-  name:
-    "req.ips, when X-Forwarded-For is present, and proxy is not trusted, should be used",
-  async fn() {
-    const mockServerRequest = createMockServerRequest();
-    const proxy = true;
-    const request = new Request(mockServerRequest, proxy);
-
-    request.headers.set("x-forwarded-for", "127.0.0.1,127.0.0.2");
-
-    assertEquals(request.ips, ["127.0.0.1", "127.0.0.2"]);
+    assertEquals(request.ip, "127.0.0.2");
   },
 });
